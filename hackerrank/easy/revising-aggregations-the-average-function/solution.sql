0f37f26d6871a7d36346aa98d7600f160e4fd69e
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-08-26 00:15:13 +0530

    feat: revising-aggregations-the-average-function (easy)

---
+++ hackerrank/easy/revising-aggregations-the-average-function/solution.sql
@@ -0,0 +1,10 @@
+
+
+
+select avg(population) from city where district='california';
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
