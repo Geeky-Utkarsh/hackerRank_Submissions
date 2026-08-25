f317264b37c15c87ad7aeab4f16c7a234a7e07a1
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-08-26 00:01:27 +0530

    feat: average-population (easy)

---
+++ hackerrank/easy/average-population/solution.sql
@@ -0,0 +1,11 @@
+
+
+
+
+select round(avg(population),0) from city; 
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
