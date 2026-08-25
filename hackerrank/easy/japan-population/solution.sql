d37f5821e77b1583b762637cb8205298f11b37b6
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-08-25 23:59:01 +0530

    feat: japan-population (easy)

---
+++ hackerrank/easy/japan-population/solution.sql
@@ -0,0 +1,9 @@
+
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+Select sum(population) from city where countrycode='jpn';
+
