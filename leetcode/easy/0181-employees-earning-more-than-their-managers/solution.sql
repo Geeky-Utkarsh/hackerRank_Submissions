58f3e68dd61cb3edb1873ae90b1d7d0089b0bc3c
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-09-10 15:15:31 +0530

    feat: employees-earning-more-than-their-managers (easy)

--- leetcode/easy/0181-employees-earning-more-than-their-managers/solution.sql
+++ leetcode/easy/0181-employees-earning-more-than-their-managers/solution.sql
@@ -2,4 +2,6 @@
 
 -- SELECT name FROM Employee where id NOT IN (SELECT name FROM Employee WHERE managerId)
 
-SELECT a.name AS Employee FROM Employee AS e INNER JOIN Employee AS a ON e.id=a.managerId WHERE a.salary>e.salary; 
+SELECT a.name AS Employee FROM Employee a
+WHERE 
+(select salary from Employee where a.managerId = id) < a.salary; 
