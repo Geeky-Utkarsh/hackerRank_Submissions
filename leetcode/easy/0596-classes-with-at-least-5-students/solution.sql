f049c17797e6889d022bc56360acc5fcaac91746
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-09-14 22:12:44 +0530

    feat: classes-with-at-least-5-students (easy)

--- leetcode/easy/0596-classes-with-at-least-5-students/solution.sql
+++ leetcode/easy/0596-classes-with-at-least-5-students/solution.sql
@@ -3,4 +3,13 @@
 -- Solution -> 1 [GROUP BY + Aggregate Function]
 -- SELECT class /* COUNT(class) */ FROM Courses WHERE COUNT(class)>5 GROUP BY class ORDER BY class DESC LIMIT 1; 
 
-SELECT class FROM Courses GROUP BY class HAVING COUNT(class)>=5 ORDER BY COUNT(class) DESC; 
+-- Solution--> 1 [GROUP BY + Aggregate Fn] based approach  
+-- SELECT class FROM Courses GROUP BY class HAVING COUNT(class)>=5 ORDER BY COUNT(class); 
+
+-- Solution--> 2 [cte based approach]
+
+with cte_name AS (
+   SELECT class FROM Courses GROUP BY class HAVING COUNT(class)>=5 ORDER BY COUNT(class) 
+)
+
+SELECT class FROM cte_name;
