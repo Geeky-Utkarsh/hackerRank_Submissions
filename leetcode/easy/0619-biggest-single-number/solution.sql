2f26bd778f320c80c8c28e2b7aeae4d566be590d
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-09-14 21:33:33 +0530

    feat: biggest-single-number (easy)

--- leetcode/easy/0619-biggest-single-number/solution.sql
+++ leetcode/easy/0619-biggest-single-number/solution.sql
@@ -8,5 +8,13 @@
 -- SELECT num, COUNT(distinct num) FROM MyNumbers GROUP BY num; 
 
 
-SELECT MAX(num) AS num FROM (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num)=1) AS F;
+-- Solution -> 1 [SubQuery Based]
+-- SELECT MAX(num) AS num FROM (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num)=1) AS F;
 
+
+-- Solution -> 2 [Using CTE]
+with cte_name AS (
+    SELECT num FROM (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num)=1) AS F
+)
+
+SELECT MAX(num) as num FROM cte_name;
