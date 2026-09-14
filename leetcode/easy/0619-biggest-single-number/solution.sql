2b821cfa931bf94202f15af2145c88d2845ee6c3
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-09-14 21:36:29 +0530

    feat: biggest-single-number (easy)

--- leetcode/easy/0619-biggest-single-number/solution.sql
+++ leetcode/easy/0619-biggest-single-number/solution.sql
@@ -1,18 +1,17 @@
 -- SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num)=1 ORDER BY num DESC limit 1;
 
 
+-- Solution -> 1 [UNION Based to Deal with NULL]
 --  SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num)=1)
     --  UNION
 --  (SELECT NULL) ORDER BY num DESC limit 1; 
 
--- SELECT num, COUNT(distinct num) FROM MyNumbers GROUP BY num; 
 
-
--- Solution -> 1 [SubQuery Based]
+-- Solution -> 2 [SubQuery Based]
 -- SELECT MAX(num) AS num FROM (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num)=1) AS F;
 
 
--- Solution -> 2 [Using CTE]
+-- Solution -> 3 [Using CTE]
 with cte_name AS (
     SELECT num FROM (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num)=1) AS F
 )
