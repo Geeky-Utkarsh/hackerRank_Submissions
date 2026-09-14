560fd6752cb0ffaf09ac0a593d385f9a7cf166cc
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2026-09-15 01:09:34 +0530

    feat: african-cities (easy)

--- hackerrank/easy/african-cities/solution.sql
+++ hackerrank/easy/african-cities/solution.sql
@@ -1,3 +1,3 @@
-SELECT SUM(city.population) FROM city
- INNER JOIN country ON city.CountryCode=country.Code 
- GROUP BY country.Continent HAVING continent='Asia';
+
+SELECT city.name FROM city
+ INNER JOIN country ON city.CountryCode=country.Code WHERE country.continent='Africa';
